43e66c986280cc5504ee0fb90df00d54bbc0da6f
moneybookCol <andres.ariza.software@hotmail.com>
2018-04-25 11:37:55 -0500

    first backend

---
+++ src/main/java/com/odiseo/odiseo/OdiseoApplication.java
@@ -0,0 +1,12 @@
+package com.odiseo.odiseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdiseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdiseoApplication.class, args);
+	}
+}
